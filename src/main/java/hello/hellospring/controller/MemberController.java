a4a7eb0b9035e20a3cac5de0fb4de0becbb2c9b7
sy2298 <sy2298@naver.com>
2022-01-27 22:15:32 +0900

    register java bean

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -6,10 +6,16 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
-    private final MemberService memberService;
+//    @Autowired private MemberService memberService;
+    private MemberService memberService;
 
-    @Autowired //스프링이 스프링 컨테이너에 있는 MemberService를 연결시켜준다.
-    public MemberController(MemberService memberService) {
+    @Autowired
+    public void setMemberService(MemberService memberService) {
         this.memberService = memberService;
+        
     }
+//    @Autowired //스프링이 스프링 컨테이너에 있는 MemberService를 연결시켜준다.
+//    public MemberController(MemberService memberService) {
+//        this.memberService = memberService;
+//    }
 }
