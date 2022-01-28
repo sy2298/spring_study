c5595f3a527661facc5a91e8c139d117e1cb0a27
sy2298 <sy2298@naver.com>
2022-01-28 16:34:20 +0900

    create post method

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -14,8 +17,22 @@ public class MemberController {
         this.memberService = memberService;
 
     }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
 //    @Autowired //스프링이 스프링 컨테이너에 있는 MemberService를 연결시켜준다.
 //    public MemberController(MemberService memberService) {
 //        this.memberService = memberService;
 //    }
+    @PostMapping("members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
 }
